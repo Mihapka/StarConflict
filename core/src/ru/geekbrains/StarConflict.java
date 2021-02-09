9813ce975aaf96278538dcea627347542aa06d8d
Mihapka <67390619+Mihapka@users.noreply.github.com>
2021-02-09 17:30:50 +0300

    lesson 1
    Co-Authored-By: Mihapka <67390619+Mihapka@users.noreply.github.com>

--- core/src/ru/geekbrains/StarConflict.java
+++ core/src/ru/geekbrains/StarConflict.java
@@ -6,26 +6,31 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarConflict extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
+    Texture backgraund;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        backgraund = new Texture("backgraund_001.jpg");
+        img = new Texture("pikachu.jpg");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(backgraund, 0, 0);
+		batch.draw(img, 20, 20);
+//        batch.draw(img, 0, 0, 20, 20, 150, 150);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
+
