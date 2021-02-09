53ae10cdc2da9036a1cf8a87d845896eaf754402
Mihapka <67390619+Mihapka@users.noreply.github.com>
2021-02-09 16:47:21 +0300

    first commit
    creation of a project
    
    Co-Authored-By: Mihapka <67390619+mihapka@users.noreply.github.com>

---
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.StarConflict;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarConflict(), config);
+	}
+}
