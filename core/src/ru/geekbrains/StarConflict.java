5a7fe911bfafaa5dfa85649dd376a6f196b7a6b5
Mihapka <67390619+mihapka@users.noreply.github.com>
2021-02-15 20:11:05 +0300

    lesson3

--- core/src/ru/geekbrains/StarConflict.java
+++ core/src/ru/geekbrains/StarConflict.java
@@ -1,36 +1,13 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import ru.geekbrains.Screen.MenuScreen;
 
-public class StarConflict extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
-    Texture backgraund;
+public class StarConflict extends Game {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        backgraund = new Texture("backgraund_001.jpg");
-        img = new Texture("pikachu.jpg");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(backgraund, 0, 0);
-		batch.draw(img, 20, 20);
-//        batch.draw(img, 0, 0, 20, 20, 150, 150);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
+        setScreen(new MenuScreen());
     }
 }
 
